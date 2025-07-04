e796895512a2496a98f3a3854cf97df4ce2c4889
Caique <caique405@gmail.com>
2025-07-04 13:42:04 -0300

    Ajustando projeto final para produção

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -60,7 +60,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
   } catch (error) {
     // If a database error occurs, return a more specific error.
     return {
-      message: 'Database Error: Failed to Create Invoice.',
+      message: error,
     };
   }
  
@@ -97,7 +97,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
 }
